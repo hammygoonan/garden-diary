330468a6cd2c7d53dfe3ed4c20bd151753562e35
Hammy Goonan <hammy@falconry.io>
2023-02-01 20:45:58 +1100

    feat: edit note.

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,19 +1,14 @@
+import { PostData } from "@/types";
+import NoteForm from "./NoteForm";
 
 type Props = {
-  data: {
-    id: string,
-    body: string,
-    date: string,
-    createdAt: string,
-    updatedAt: string,
-  }[]
+  data: PostData[],
 };
 
 export default function PostList({ data }: Props) {
-
   return (
     <div>
-      {data && data.map((post) => (
+      {data && data.map((post, idx) => (
         <div className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box" key={post.id}>
           <input type="checkbox" />
           <div className="collapse-title text-xl font-medium">
@@ -22,9 +17,18 @@ export default function PostList({ data }: Props) {
           <div className="collapse-content">
             {post.body}
           </div>
+
+          <label htmlFor={`edit-note=${idx}`} className="btn">Edit note</label>
+          <input type="checkbox" id={`edit-note=${idx}`} className="modal-toggle" />
+          <div className="modal">
+            <div className="modal-box">
+              <NoteForm id={`edit-note=${idx}`} data={post} />
+            </div>
+          </div>
         </div>
-      ))}
-    </div>
+      ))
+      }
+    </div >
   )
 }
 
